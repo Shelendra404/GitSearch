1fa997d51445066b00905d662eeac7f381c5641c
Shelendra404 <niina.jarvinen@gmail.com>
2020-05-13 10:19:10 +0300

    moved <PublicRepositories> code back into app.js in order to test getting commits based on given repo (works here).

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import UserSearch from './components/user-search/user-search.component';
-import PublicRepositories from './components/public-repositories/public-repositories.component';
+//import PublicRepositories from './components/public-repositories/public-repositories.component';
 import GetUserInfo from './components/get-user-info/get-user-info.component';
 import './App.css';
 
@@ -20,19 +20,31 @@ class App extends React.Component {
     this.setState({ username: props });
   }
 
+  getCredentials(props) {
+    const apiUser = process.env.REACT_APP_USER;
+    const apiKey = process.env.REACT_APP_TOKEN;
+
+    if (props === 'user') {
+      return apiUser;
+    } else if (props === 'key') {
+      return apiKey;
+    }
+  }
+
   getUserData(event) {
     event.preventDefault();
 
     const getUserRepos = async () => {
       try {
-        const apiUser = process.env.REACT_APP_USER;
-        const apiKey = process.env.REACT_APP_TOKEN;
-
         const users = await fetch(
           `https://api.github.com/users/${this.state.username}`,
           {
             headers: new Headers({
-              Authorization: 'Basic ' + btoa(`${apiUser}:${apiKey}`),
+              Authorization:
+                'Basic ' +
+                btoa(
+                  `${this.getCredentials('user')}:${this.getCredentials('key')}`
+                ),
             }),
           }
         );
@@ -40,13 +52,16 @@ class App extends React.Component {
         this.setState({ userdata: userData });
         const userRepos = await fetch(userData.repos_url, {
           headers: new Headers({
-            Authorization: 'Basic ' + btoa(`${apiUser}:${apiKey}`),
+            Authorization:
+              'Basic ' +
+              btoa(
+                `${this.getCredentials('user')}:${this.getCredentials('key')}`
+              ),
           }),
         });
         const repoData = await userRepos.json();
 
         this.setState({ repos: repoData });
-        //getUserCommits();
       } catch (err) {
         console.log('Something went wrong: ' + err);
       }
@@ -54,6 +69,32 @@ class App extends React.Component {
 
     getUserRepos();
   }
+
+  getCommits = (props) => {
+    console.log('You clicked on Show Commits from ' + props);
+    const getUserCommits = async () => {
+      try {
+        const commits = await fetch(
+          `https://api.github.com/repos/${this.state.userdata.login}/${props}/commits`,
+          {
+            headers: new Headers({
+              Authorization:
+                'Basic ' +
+                btoa(
+                  `${this.getCredentials('user')}:${this.getCredentials('key')}`
+                ),
+            }),
+          }
+        );
+        const commitData = await commits.json();
+        console.log(commitData);
+      } catch (err) {
+        console.log('Something went wrong while fetching the commits.');
+      }
+    };
+
+    getUserCommits();
+  };
 
   showUserRepos = () => {
     this.setState({ showRepos: true });
@@ -76,9 +117,27 @@ class App extends React.Component {
 
     if (this.state.showRepos) {
       repos = (
-        <PublicRepositories
-          user={this.state.username}
-          repo={this.state.repos}></PublicRepositories>
+        <div className='public-repositories'>
+          <h2>Public repositories from user {this.state.username}</h2>
+
+          {this.state.repos.map((repository) => (
+            <div key={repository.id}>
+              <a
+                href={repository.html_url}
+                target='_blank'
+                rel='noreferrer noopener'>
+                {repository.name}
+              </a>
+              <button onClick={() => this.getCommits(repository.name)}>
+                Get Commits
+              </button>
+              <br />
+            </div>
+          ))}
+        </div>
+        //   <PublicRepositories
+        //     user={this.state.username}
+        //     repo={this.state.repos}></PublicRepositories>
       );
     }
 
